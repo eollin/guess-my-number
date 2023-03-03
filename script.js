3297c2eb4e83fec99455dc14c53f5ae2a6c78154
Kate <eydos@list.ru>
2023-03-03 14:18:56 +0100

    did code refactoring

--- script.js
+++ script.js
@@ -4,17 +4,21 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = (message) => {
+  document.querySelector('.message').textContent = message;
+};
+
+const displayScore = (score) => {
+  document.querySelector('.score').textContent = score;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
-  //   When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No number';
-
-    //   When player wins
+    displayMessage('⛔ No number');
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = '🥳 Correct number!';
-
+    displayMessage('🥳 Correct number!');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -23,27 +27,14 @@ document.querySelector('.check').addEventListener('click', function () {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
-
-    //   When guess is to high
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too high';
+      displayMessage(guess > secretNumber ? '📈 Too high' : '📉 Too low');
       score--;
-      document.querySelector('.score').textContent = score;
+      displayScore(score);
     } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!';
-      document.querySelector('.score').textContent = 0;
-    }
-
-    //   When guess is to low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too low';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '💥 You lost the game!';
-      document.querySelector('.score').textContent = 0;
+      displayMessage('💥 You lost the game!');
+      displayScore(0);
     }
   }
 });
@@ -52,9 +43,9 @@ document.querySelector('.again').addEventListener('click', function () {
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
 
-  document.querySelector('.score').textContent = score;
+  displayScore(score);
+  displayMessage('Start guessing...!');
   document.querySelector('.number').textContent = '?';
-  document.querySelector('.message').textContent = 'Start guessing...!';
   document.querySelector('.guess').value = '';
 
   document.querySelector('body').style.backgroundColor = '#222';
