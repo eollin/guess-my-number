b36369a3e840f80c4f121b6d0549e62717a6b6df
Kate <eydos@list.ru>
2023-03-03 13:41:50 +0100

    implemented highscore

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -13,9 +14,15 @@ document.querySelector('.check').addEventListener('click', function () {
     //   When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🥳 Correct number!';
+
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     //   When guess is to high
   } else if (guess > secretNumber) {
