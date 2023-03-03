bebc5590aa5ed90c99c43aa306362c427bb80378
Kate <eydos@list.ru>
2023-03-03 12:12:04 +0100

    added winning css changes

--- script.js
+++ script.js
@@ -8,10 +8,17 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //   When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number';
+
+    //   When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🥳 Correct number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    //   When guess is to high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high';
@@ -21,6 +28,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
     }
+
+    //   When guess is to low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too low';
